558ecbf018b60ea014c46e516c195b9e5804ba38
Zibi Braniecki <zibi@braniecki.net>
2019-08-09 20:15:44 -0700

    Add basic lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! tinystr is a small ASCII-only bounded length string representation.
+//!
+//! # Example
+//!
+//! ```
+//! use tinystr::{TinyStr4, TinyStr8};
+//!
+//! fn main() {
+//!     let s1: TinyStr4 = "tEsT".parse()
+//!         .expect("Failed to parse.");
+//!
+//!     assert_eq!(s1, "tEsT");
+//!     assert_eq!(s1.to_ascii_uppercase(), "TEST");
+//!     assert_eq!(s1.to_ascii_lowercase(), "test");
+//!     assert_eq!(s1.to_ascii_titlecase(), "Test");
+//!     assert_eq!(s1.is_ascii_alphanumeric(), true);
+//!
+//!     let s2: TinyStr8 = "New York".parse()
+//!         .expect("Failed to parse.");
+//!
+//!     assert_eq!(s2, "New York");
+//!     assert_eq!(s2.to_ascii_uppercase(), "NEW YORK");
+//!     assert_eq!(s2.to_ascii_lowercase(), "new york");
+//!     assert_eq!(s2.is_ascii_alphanumeric(), false);
+//! }
+//! ```
 mod helpers;
 mod tinystr4;
 mod tinystr8;
